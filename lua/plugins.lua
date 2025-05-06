67a2e6393268eb9c435408005a271255546f8244
aspire5 <jgoo11177@gmail.com>
2025-05-06 13:50:46 +0800

    create keymappng

---
+++ lua/plugins.lua
@@ -0,0 +1,18 @@
+return {
+    {'https://github.com/SirVer/ultisnips'},
+    {'https://github.com/honza/vim-snippets'},
+    {'https://github.com/jiangmiao/auto-pairs'},
+    {'https://github.com/tpope/vim-surround'},
+    {'https://github.com/tpope/vim-commentary'},
+    {'mhinz/vim-startify'},
+    {'vim-airline/vim-airline'},
+    {'vim-airline/vim-airline-themes'},
+
+
+    {'nvim-lua/plenary.nvim'},
+    {'nvim-telescope/telescope.nvim',  tag = '0.1.8' },
+    {'nvim-tree/nvim-web-devicons'},
+    {'MunifTanjim/nui.nvim'},
+    {'nvim-neo-tree/neo-tree.nvim'},
+    {'folke/lazy.nvim'},
+}
