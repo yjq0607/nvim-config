c4c7dcc133d9050063257768a267f8c5148e740a
aspire5 <jgoo11177@gmail.com>
2025-06-02 18:08:32 +0800

    markdown preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,6 +12,14 @@ return {
     {'nvim-lua/plenary.nvim'},
     {'nvim-telescope/telescope.nvim',  tag = '0.1.8' },
     {'nvim-tree/nvim-web-devicons'},
+    {'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
     {'MunifTanjim/nui.nvim'},
     {'nvim-neo-tree/neo-tree.nvim'},
     {'folke/lazy.nvim'},
